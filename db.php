90002a66bda5d8a59b0cc9f07fa2e6cb47ae19d1
Mohamed_13 <mohamed.zenbout@laplateforme.io>
2025-12-27 19:09:31 +0100

    création de la base de donnée

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";       
+$db   = "oclock";          
+$user = "root";        
+$pass = "";        
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
